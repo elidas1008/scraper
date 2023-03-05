b32c134cb158d7579f92479a9c715e4923613289
Eli Das <elidas1008@gmail.com>
2023-03-05 15:39:55 +0100

    api client: serializer

--- src/Lib/AbstractSerializer.php
+++ src/Lib/AbstractSerializer.php
@@ -2,7 +2,10 @@
 
 namespace Elidas1008\Experiments\Lib;
 
+use Elidas1008\Experiments\Lib\Entity\FieldName;
 use Elidas1008\Experiments\Lib\Entity\Serializable;
+use ReflectionClass;
+use RuntimeException;
 
 abstract class AbstractSerializer
 {
@@ -14,4 +17,26 @@ abstract class AbstractSerializer
     }
 
     abstract public function deserialize(string $encoded, string $entityName): Serializable;
+
+    protected function createEntity(string $entityName): Serializable
+    {
+        if (false === is_subclass_of($entityName, Serializable::class)) {
+            throw new RuntimeException('Argument \'entity\' must extend Serializable');
+        }
+
+        return new $entityName();
+    }
+
+    protected function hydrate(array $data, Serializable $entity)
+    {
+        $r = new ReflectionClass($entity);
+        foreach ($r->getProperties() as $property) {
+            $arguments = $property->getAttributes(FieldName::class)[0]?->getArguments() ?? null;
+
+            $dataKey = $arguments['fieldName'] ?? $property->getName();
+            $entityKey = $property->getName();
+
+            $entity->$entityKey = $data[$dataKey] ?? '';
+        }
+    }
 }
--- src/Lib/JsonSerializer.php
+++ src/Lib/JsonSerializer.php
@@ -4,10 +4,7 @@ declare(strict_types=1);
 
 namespace Elidas1008\Experiments\Lib;
 
-use Elidas1008\Experiments\Lib\Entity\FieldName;
 use Elidas1008\Experiments\Lib\Entity\Serializable;
-use ReflectionClass;
-use RuntimeException;
 use Throwable;
 
 class JsonSerializer extends AbstractSerializer
@@ -17,35 +14,11 @@ class JsonSerializer extends AbstractSerializer
      */
     public function deserialize(string $encoded, string $entityName): Serializable
     {
-        if (false === is_subclass_of($entityName, Serializable::class)) {
-            throw new RuntimeException('Argument \'entity\' must extend Serializable');
-        }
-        $entity = new $entityName();
+        $entity = $this->createEntity($entityName);
 
         $data = json_decode($encoded, true, JSON_THROW_ON_ERROR);
 
-        $r = new ReflectionClass($entity);
-        foreach ($r->getProperties() as $property) {
-            $arguments = $property->getAttributes(FieldName::class)[0]?->getArguments() ?? null;
-
-            $dataKey = $arguments['fieldName'] ?? $property->getName();
-            $entityKey = $property->getName();
-
-            $entity->$entityKey = $data[$dataKey] ?? '';
-        }
-//
-//        foreach ($data as $key => $value) {
-//            if (in_array(AbstractSerializerOptions::THROW_ON_MISSING_PROPERTY, $this->options)
-//                && false === property_exists($entity, $key)) {
-//                throw new RuntimeException("Object '" . $entity::class . "' does not have property '" . $key . "'");
-//            }
-//
-//            if (false === property_exists($entity, $key)) {
-//                continue;
-//            }
-//
-//            $entity->$key = $value;
-//        }
+        $this->hydrate($data, $entity);
 
         return $entity;
     }
